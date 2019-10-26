7d35990e93ac1521db46d76258fa6447c9869594
ubuntu <maheshkrishnachalla@gmail.com>
2019-10-26 07:47:31 +0000

    create the archistar Maven project

---
+++ achistar/src/main/java/com/achistar/App.java
@@ -0,0 +1,13 @@
+package com.achistar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Achistar Technologies Project successfully deployed into Docker" );
+    }
+}
