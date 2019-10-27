9ca7451da8f32e8ea084dbcbf326c34796fcdf74
ubuntu <maheshkrishnachalla@gmail.com>
2019-10-27 13:57:25 +0000

    Added one file and updates App main class

--- achistar/src/main/java/com/achistar/App.java
+++ achistar/src/main/java/com/achistar/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Achistar Technologies Project successfully deployed into Docker" );
+        System.out.println( "Achistar Technologies Project successfully deployed into Docker using Jenkins Build" );
     }
 }
