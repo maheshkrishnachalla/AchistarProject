56ee993d940936cff36cd39b1a6331b89512d6d6
ubuntu <maheshkrishnachalla@gmail.com>
2019-10-27 14:46:30 +0000

    Aded one jave class and one test class, updates junit to 4

---
+++ achistar/src/main/java/com/achistar/Calculations.java
@@ -0,0 +1,44 @@
+package com.achistar;
+
+public class Calculations {
+
+	/**
+	 *  add method take two inputs and return sum of two inputs
+	 * @param a
+	 * @param b
+	 * @return
+ 	 */
+	public double add(double a, double b) {
+		return a+b;
+	}
+	
+	/**
+	 * mul method take two inputs and return product of two inputs
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double mul(double a, double b) {
+		return a*b;
+	}
+	
+	/**
+	 * sub method take two inputs and return subtraction of two inputs
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double sub(double a, double b) {
+		return a-b;
+	}
+	
+	/**
+	 * div method take two inputs and return division of two inputs
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double div(double a, double b) {
+		return a/b;
+	}
+}
---
+++ achistar/src/test/java/com/achistar/CalculationsTest.java
@@ -0,0 +1,60 @@
+package com.achistar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CalculationsTest {
+
+	Calculations calculations=new Calculations();
+	
+	@Test
+	public void addTest() {
+		double a=2;
+		double b=7;
+		boolean status=false;
+		if(calculations.add(a,b)==a+b)
+		{
+			status=true;
+		}
+		assertTrue(status);
+	}
+	
+	@Test
+	public void mulTest() {
+		double a=2;
+		double b=7;
+		boolean status=false;
+		if(calculations.mul(a,b)==a*b)
+		{
+			status=true;
+		}
+		assertTrue(status);
+	}
+	
+	@Test
+	public void subTest() {
+		double a=2;
+		double b=7;
+		boolean status=false;
+		if(calculations.sub(a,b)==a-b)
+		{
+			status=true;
+		}
+		assertTrue(status);
+		
+	}
+	
+	@Test
+	public void divTest() {
+		double a=2;
+		double b=7;
+		boolean status=false;
+		if(calculations.div(a,b)==a/b)
+		{
+			status=true;
+		}
+		assertTrue(status);
+	}
+	
+}
